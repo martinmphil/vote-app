31fffe125c859a1b9489bb4ea1913a3f6c77f637
Martin <martin.mphil@gmail.com>
2019-06-25 10:41:32 +0100

    useCallback for checkCloudFn

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useCallback, useEffect } from 'react'
 import db from './private/privateData'
 import './App.css'
 import ClearVotesButton from './ClearVotesButton'
@@ -39,71 +39,43 @@ function App() {
   // Set App state.
   const [user, setUser] = useState( commons[0] )
   const [votes, setVotes] = useState([])
-  const [askCloud, setAskCloud] = useState(false)
 
-  useEffect( () => {
-    // To REMOVE
-    console.log('checking for new votes')
+  // Setting local votes to match cloud data.
+  const checkCloudFn = useCallback( () => {
     const fetchData = async () => {
       const cloudData = await(
         db.collection('an_organiser').doc('2019-06-17T09:22:33.456Z').collection('commons')
         .get().then( querySnapshot =>
           querySnapshot.docs.map( doc => doc.data().v )
-        )
+        ).catch(function(error) {console.log("Error reading cloud data:", error)})
       )
-      // To REMOVE
-      console.log(cloudData.slice(0, commons.length) )
       setVotes( cloudData.slice(0, commons.length) )      
     }
     fetchData()
-  }, [commons.length, askCloud])
-  // Calling checkCloudFn changes askCloud dependency which triggers an update from the cloud.
-  function checkCloudFn() {
-    setAskCloud(true)
-    console.log('setting ask cloud to true')
-  }
+  }, [commons.length])
 
+  // Check cloud on start-up.
+  useEffect( () => {
+    checkCloudFn()
+  },[checkCloudFn])
 
-
-  // CONVERT TO ASNYC AWAIT
-  const sendVoteToCloud = (userIndex, candidateIndex) => {
-
-    // To REMOVE
-    console.log('sending votes to the cloud');
-    
-
+  const handleVote = async (candidateIndex) => {
+    const userIndex = commons.indexOf(user)
     const voteRef =
       db.collection('an_organiser').doc('2019-06-17T09:22:33.456Z')
       .collection('commons').doc( userIndex.toString() )
 
-    voteRef.update({
-      v: candidateIndex
-    })
-    .catch(function(err) {
-      console.log('Error setting cloud db:- ', err)
-    })
-  }
+    await voteRef.update({v: candidateIndex})
+      .catch(function(err) {console.log('Error setting cloud db:- ', err)})
 
-  const handleVote = (candidateIndex) => {
-    const userIndex = commons.indexOf(user)
-    // Set votes by mapping votes array unchanged except at
-    // the index number for this user in the commons
-    // where this single value is updated to the index number of their preferred candidate.
-
-    // To REMOVE
-    console.log('handling vote');
-    
+    // Set local votes by mapping votes and only changing value
+    // at the index number for this user in the commons.
     setVotes(
       votes.map( (v, index) => {return ( index === userIndex ? candidateIndex : v ) })
     )
-
-    sendVoteToCloud( (userIndex.toString()), candidateIndex)
+    
+    window.scrollTo(0,0);
   }
-
-
-
-
-
 
   const handleLogin = (e) => { setUser(e.target.value) }
 
@@ -144,7 +116,7 @@ function App() {
 
       <main>
         <h1>Vote app</h1>
-        <h2>{topic}</h2>
+        <h2 id="topicHeading">{topic}</h2>
         <MainXhtml />
         <hr />
       </main>
