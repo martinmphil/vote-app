c6b38ceb9a0519fd75de67a0aef005adcbae17ae
Martin <martin.mphil@gmail.com>
2019-08-04 12:35:19 +0100

    LeagueTable returns single expression

--- src/App.js
+++ src/App.js
@@ -1,34 +1,32 @@
-import React, { useState, useEffect } from 'react'
-import db from './private/privateData'
-import './App.css'
-import LogIn from './LogIn'
-import BallotPaper from './BallotPaper'
-import LeagueTable from './LeagueTable'
-import CastVotes from './CastVotes'
-import ClearVotesButton from './ClearVotesButton'
+import React, { useState, useEffect } from "react";
+import db from "./private/privateData";
+import "./App.css";
+import LogIn from "./LogIn";
+import BallotPaper from "./BallotPaper";
+import LeagueTable from "./LeagueTable";
+import CastVotes from "./CastVotes";
+import ClearVotesButton from "./ClearVotesButton";
 
 // NB snapshot.docs[0].data().v and voteIndex: docs.id, ...docs.data().v
 
 function useCloud() {
-  const [data, setData] = useState([])
+  const [data, setData] = useState([]);
 
-  useEffect( () => {
+  useEffect(() => {
     const unsubscribeFn = db
-      .collection('an_organiser')
-      .doc('2019-06-17T09:22:33.456Z')
-      .collection('commons')
+      .collection("an_organiser")
+      .doc("2019-06-17T09:22:33.456Z")
+      .collection("commons")
       .onSnapshot(snapshot => {
-        const cloudData = snapshot.docs.map( docs => 
-          docs.data().v
-        )
-        setData(cloudData)
-      })
-    return () => unsubscribeFn()
-  }, [])
-  return data
+        const cloudData = snapshot.docs.map(docs => docs.data().v);
+        setData(cloudData);
+      });
+    return () => unsubscribeFn();
+  }, []);
+  return data;
 }
 
-const topic = "Framework for developing new Testers' App."
+const topic = "Framework for developing new Testers' App.";
 
 const candidates = [
   "Function components in create-react-app",
@@ -40,160 +38,143 @@ const candidates = [
   "Angular",
   "Elm",
   "Vanilla JavaScript"
-]
+];
 
-const commons = [
-  "Mar",
-  "Star",
-  "York",
-  "Flff",
-  "Crit",
-  "Baz"
-]
+const commons = ["Mar", "Star", "York", "Flff", "Crit", "Baz"];
 
-  // Index of votes array matches index of commons array,
-  // thus correlating cast votes to specific users.
-  // The cloud database presets votes array elements to a value of 999999.
-  // When casting a vote, the votes array records 
-  // the index value of their preference from the candidate array.
+// Index of votes array matches index of commons array,
+// thus correlating cast votes to specific users.
+// The cloud database presets votes array elements to a value of 999999.
+// When casting a vote, the votes array records
+// the index value of their preference from the candidate array.
 
 function App() {
-
   // Set app state.
-  const votes = useCloud()
-  const [user, setUser] = useState(commons[0])
-  const [showWinner, setShowWinner] = useState(false)
-  const [organiser, setOrganiser] = useState(false)
+  const votes = useCloud();
+  const [user, setUser] = useState(commons[0]);
+  const [showWinner, setShowWinner] = useState(false);
+  const [organiser, setOrganiser] = useState(false);
 
   // This function checks if this user already voted.
-  // useEffect( ()=>{
+  // useEffect( () => {
   //   if (votes[commons.indexOf(user)] < 999999)
   //     {setShowWinner(true)}
   // }, [votes, user])
 
   // Handle removing the results overlay.
   const clickToHideWinnerFn = () => {
-    setShowWinner(false)
-  }
+    setShowWinner(false);
+  };
 
   // Handle clicking background to remove the results overlay.
-  useEffect( () => {
-    const clickingAwayFromResults = (e) => {
+  useEffect(() => {
+    const clickingAwayFromResults = e => {
       // Test if results overlay doesn't contain target.
-      if ((!(document.querySelector('.results-overlay').contains(e.target) )))
-        {clickToHideWinnerFn()}
-    }
+      if (!document.querySelector(".results-overlay").contains(e.target)) {
+        clickToHideWinnerFn();
+      }
+    };
     if (showWinner === true) {
-      window.addEventListener('click', clickingAwayFromResults, false )
+      window.addEventListener("click", clickingAwayFromResults, false);
     }
     return () => {
-      window.removeEventListener('click', clickingAwayFromResults, false )
-    }
-  }, [showWinner])
+      window.removeEventListener("click", clickingAwayFromResults, false);
+    };
+  }, [showWinner]);
 
   const toggleOrganiser = () => {
-    organiser ? setOrganiser(false) : setOrganiser(true)
-  }
+    organiser ? setOrganiser(false) : setOrganiser(true);
+  };
 
-  const handleVote = async (candidateIndex) => {
-    const userIndex = commons.indexOf(user)
+  const handleVote = async candidateIndex => {
+    const userIndex = commons.indexOf(user);
     const voteRef = db
-      .collection('an_organiser').doc('2019-06-17T09:22:33.456Z')
-      .collection('commons').doc( userIndex.toString() )
-    await voteRef.update({v: candidateIndex})
-      .catch(function(err) {console.log('Error setting cloud db:- ', err)});
-    setShowWinner(true)
-  }
+      .collection("an_organiser")
+      .doc("2019-06-17T09:22:33.456Z")
+      .collection("commons")
+      .doc(userIndex.toString());
+    await voteRef.update({ v: candidateIndex }).catch(function(err) {
+      console.log("Error setting cloud db:- ", err);
+    });
+    setShowWinner(true);
+  };
 
   // Change user.
-  const handleLogin = (e) => {
+  const handleLogin = e => {
     // Excude dummy "Change user" option with zero length value "".
     if (e.target.value.length > 0) {
-      setUser(e.target.value)
+      setUser(e.target.value);
     }
     if (votes[commons.indexOf(e.target.value)] === 999999) {
-      setShowWinner(false)
+      setShowWinner(false);
     }
-  }
-
+  };
 
   return (
     <div className="App">
-
       <LeagueTable
-        votes = {votes}
-        candidates = {candidates}
-        topic = {topic}
-        clickToHideWinnerFn = {clickToHideWinnerFn}
-        commons = {commons}
-        user = {user}
-        handleLogin = {handleLogin}
-        showWinner = {showWinner}
+        votes={votes}
+        candidates={candidates}
+        topic={topic}
+        clickToHideWinnerFn={clickToHideWinnerFn}
+        commons={commons}
+        user={user}
+        handleLogin={handleLogin}
+        showWinner={showWinner}
       />
 
       <header>
         {// Either un-cast vote, or votes still loading, present a consistent view for fresh user.
-          (votes[commons.indexOf(user)] === 999999) || votes.length < commons.length ?
-          <span>Cast your vote.</span> :
-          <button
-            className = "secondary-button"
-            type="button"
-            onClick={ () => setShowWinner(true) }
-          >
+        votes[commons.indexOf(user)] === 999999 || votes.length < commons.length ? (
+          <span>Cast your vote.</span>
+        ) : (
+          <button className="secondary-button" type="button" onClick={() => setShowWinner(true)}>
             Show results
           </button>
-        }
-        <LogIn
-          commons = {commons}
-          user = {user}
-          handleLogin = {handleLogin}
-        />
+        )}
+        <LogIn commons={commons} user={user} handleLogin={handleLogin} />
       </header>
 
       <main
         // onClick = {() => setShowWinner(false)}
-        className = {showWinner ? 'misty' : ''}
+        className={showWinner ? "misty" : ""}
       >
         {// Same class style avoids flicker when loaded.
-          (!(votes.length >= commons.length)) ? 
-          <p className="subtle-heading">Loading...</p> :
+        !(votes.length >= commons.length) ? (
+          <p className="subtle-heading">Loading...</p>
+        ) : (
           <h1 className="subtle-heading">Vote app</h1>
-        }
+        )}
 
         {/* <Spinner votes={votes} commons={commons}/> */}
 
         <BallotPaper
-          user = {user}
-          votes = {votes}
-          commons = {commons}
-          handleVote = {handleVote}
-          candidates = {candidates}
-          topic = {topic}
-          clickToHideWinnerFn = {clickToHideWinnerFn}
-          showWinner = {showWinner}
+          user={user}
+          votes={votes}
+          commons={commons}
+          handleVote={handleVote}
+          candidates={candidates}
+          topic={topic}
+          clickToHideWinnerFn={clickToHideWinnerFn}
+          showWinner={showWinner}
         />
         <hr />
 
         {//NB true && expression evaluates to expression; false && expression evaluates to false.
-          (votes[commons.indexOf(user)] < 999999) &&
-          <CastVotes
-            commons = {commons}
-            candidates = {candidates}
-            votes = {votes}
-          />
-        }
+        votes[commons.indexOf(user)] < 999999 && (
+          <CastVotes commons={commons} candidates={candidates} votes={votes} />
+        )}
 
         <ClearVotesButton
-          commons = {commons}
-          organiser = {organiser}
-          toggleOrganiser = {toggleOrganiser}
-          clickToHideWinnerFn = {clickToHideWinnerFn}
+          commons={commons}
+          organiser={organiser}
+          toggleOrganiser={toggleOrganiser}
+          clickToHideWinnerFn={clickToHideWinnerFn}
         />
         <hr />
       </main>
-
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/LeagueTable.js
+++ src/LeagueTable.js
@@ -1,81 +1,76 @@
-import React from 'react'
-import LogIn from './LogIn'
+import React from "react";
+import LogIn from "./LogIn";
 
-function LeagueTable (props) {
+function LeagueTable(props) {
+  // Extra cloud entires produce a votes array too long for this commons.
+  // If needed, fix with votes.slice(0, props.commons.length)
+  // Also fixed by sorting popularity > 0
 
-  // Extra cloud entires produce a votes array too long for this commons
-  // hence votes.slice(0, props.commons.length)
-
-  // pollResults, ordered by popularity, 
+  // pollResults, ordered by popularity,
   // contains array of {candidate: "candidate_x", popularity: number}
-  const pollResults = props.candidates.map(
-    (c, index) => { return (
-      {candidate: c, popularity: (props.votes.filter( v => v === index ).length ) }
-    )})
+  const pollResults = props.candidates
+    .map((c, index) => {
+      return { candidate: c, popularity: props.votes.filter(v => v === index).length };
+    })
     .filter(p => p.popularity > 0)
-    .sort( (a, b) => b.popularity - a.popularity )
+    .sort((a, b) => b.popularity - a.popularity);
 
-  function listings (array) {
-    return array.map(d => 
+  function listings(array) {
+    return array.map(d => (
       <li key={d.candidate}>
         "{d.candidate}" gained {d.popularity}
-        {d.popularity === 1 ? ' vote' : ' votes'}
+        {d.popularity === 1 ? " vote" : " votes"}
       </li>
-    )
+    ));
   }
 
-  function ResultsHeader () {
+  function ResultsHeader() {
     return (
       <div className="results-overlay-header">
-        <button
-          onClick={ () => props.clickToHideWinnerFn() }
-        >
-          Close results
-        </button>
-        <LogIn
-          commons = {props.commons}
-          user = {props.user}
-          handleLogin = {props.handleLogin}
-        />
+        <button onClick={() => props.clickToHideWinnerFn()}>Close results</button>
+        <LogIn commons={props.commons} user={props.user} handleLogin={props.handleLogin} />
       </div>
-    )
+    );
   }
 
-  if (props.votes.length < props.commons.length)
-  {return <span></span>}
-    
-  // test for tied first place
-  else if (pollResults.length > 1
-    && pollResults[0].popularity === pollResults[1].popularity)
-  {
-    let runnersUp = pollResults.filter(j => j.popularity !== pollResults[0].popularity)
-
-    // NB className "results-overlay" is used to detect if user clicks background to remove resutls overlay.
-    return (
-      <section className={'results-overlay ' + (props.showWinner ? 'slide-down' : 'slide-up') }>
-        <ResultsHeader />
-        <p>Front runners:-</p>
-        <ul>
-          {listings(pollResults.filter(j => j.popularity === pollResults[0].popularity) )}
-        </ul>
-        {(runnersUp.length > 0) && <p>{runnersUp.length === 1 ? 'Runner' : 'Runners'} up:-</p>}
-        <ul>{listings(runnersUp)}</ul>
-      </section>
-    )
-  } else {
-    let alsoRan = pollResults.slice(1)
-    return (
-      <section className={'results-overlay ' + (props.showWinner ? 'slide-down' : 'slide-up')}>
-        <ResultsHeader />
-        <p>Front runner:-</p>
-        <ul>
-          { (pollResults.length > 0) && listings( pollResults.slice(0,1) ) }
-        </ul>
-        {(alsoRan.length > 0) && <p>{alsoRan.length === 1 ? 'Runner' : 'Runners'} up:-</p>}
-        <ul>{listings(alsoRan)}</ul>
-      </section>
-    )
+  function Results() {
+    // test for tied first place
+    if (pollResults.length > 1 && pollResults[0].popularity === pollResults[1].popularity) {
+      let runnersUp = pollResults.filter(j => j.popularity !== pollResults[0].popularity);
+      return (
+        <div>
+          <p>Front runners:-</p>
+          <ul>{listings(pollResults.filter(j => j.popularity === pollResults[0].popularity))}</ul>
+          {runnersUp.length > 0 && <p>{runnersUp.length === 1 ? "Runner" : "Runners"} up:-</p>}
+          <ul>{listings(runnersUp)}</ul>
+        </div>
+      );
+    } else {
+      let alsoRan = pollResults.slice(1);
+      return (
+        <div>
+          <p>Front runner:-</p>
+          <ul>{pollResults.length > 0 && listings(pollResults.slice(0, 1))}</ul>
+          {alsoRan.length > 0 && <p>{alsoRan.length === 1 ? "Runner" : "Runners"} up:-</p>}
+          <ul>{listings(alsoRan)}</ul>
+        </div>
+      );
+    }
   }
+
+  return (
+    <section
+      className={
+        "results-overlay " +
+        (props.showWinner ? "slide-down" : "slide-up") +
+        // Hide results if data still loading from cloud.
+        (props.votes.length < props.commons.length ? "hidden" : "")
+      }
+    >
+      <ResultsHeader />
+      <Results />
+    </section>
+  );
 }
 
-export default LeagueTable
+export default LeagueTable;
